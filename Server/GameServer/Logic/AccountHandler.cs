958b0a683ea1977c036ed953041392aa18461b98
marssuren <287815741@qq.com>
2018-01-09 00:26:58 +0800

    调整部分服务器环境

--- Server/GameServer/Logic/AccountHandler.cs
+++ Server/GameServer/Logic/AccountHandler.cs
@@ -9,23 +9,39 @@ namespace GameServer.Logic
 {
 	public class AccountHandler : IHandler //帐号处理的逻辑层
 	{
+		public void OnDisconnect(ClientPeer _clientPeer)
+		{
+			throw new NotImplementedException();
+		}
+
 		public void OnReceive(ClientPeer _clientPeer, int _subCode, object _value)
 		{
 			switch(_subCode)
 			{
 				case AccountCode.Regist_ClientReq:
-				Console.WriteLine("");
+				{
+					AccountDto tAccountDto = _value as AccountDto;
+					Console.WriteLine(tAccountDto.Account);
+					Console.WriteLine(tAccountDto.Password);
+				}
 				break;
 				case AccountCode.Login:
+				{
+					AccountDto tAccountDto = _value as AccountDto;
+					Console.WriteLine(tAccountDto.Account);
+					Console.WriteLine(tAccountDto.Password);
+				}
 				break;
 				default:
 				break;
 			}
+
+
 		}
 
-		public void OnDisconnect(ClientPeer _clientPeer)
-		{
-			throw new NotImplementedException();
-		}
 	}
 }
+
+
+
+
