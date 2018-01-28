ff9881aa6bb4260013d3fc9145bf70d80f74e5a0
marssuren <287815741@qq.com>
2018-01-28 22:39:17 +0800

    处理进入房间玩家位置相关

--- Server/Protocol/Code/Dto/MatchRoomDto.cs
+++ Server/Protocol/Code/Dto/MatchRoomDto.cs
@@ -10,6 +10,10 @@ namespace Protocol.Code.Dto
 	{
 		public Dictionary<int, PlayerDto> UidUserDic;   //用户uid对应的用户数据传输模型
 		public List<int> ReadyUidLst;       //准备的玩家id列表
+		public List<int> UidList=new List<int>();			//房间内的玩家Uid
+		public int LeftPlayerId;			//左边玩家Id
+		public int OppsitePlayerId;			//对面玩家Id
+		public int RightPlayerId;			//右边玩家Id
 		public MatchRoomDto()
 		{
 			UidUserDic = new Dictionary<int, PlayerDto>();
@@ -18,15 +22,22 @@ namespace Protocol.Code.Dto
 		public void Add(PlayerDto _newPlayer)		//玩家进入
 		{
 			UidUserDic.Add(_newPlayer.Id, _newPlayer);
+			UidList.Add(_newPlayer.Id);
 		}
-		public void Remove(PlayerDto _newPlayer)	//移除玩家
+		public void Leave(PlayerDto _player)	//移除玩家
 		{
-			UidUserDic.Remove(_newPlayer.Id);
+			UidUserDic.Remove(_player.Id);
+
 		}
 		public void Ready(int _uid)					//玩家准备
 		{
 			ReadyUidLst.Add(_uid);
 		}
-
+		public void ResetPosition()					//重置位置，在每次玩家进入或者离开房间的时候都需要调整玩家位置
+		{
+			LeftPlayerId = -1;
+			RightPlayerId = -1;
+			OppsitePlayerId = -1;
+		}
 	}
 }
